2980b3c5eac6fbd92cce965beef49b05a9fa7f4a
Matheus Custodio Santos <44846245837@info.local>
2018-12-06 14:18:31 -0200

    Começando o BE do comentarios

--- Controllers/ComentarioController.cs
+++ Controllers/ComentarioController.cs
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-using System;
->>>>>>> 80754a9ef13e3b31921480b933bee5108c855348
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Senai.Projeto.Carfel.Models;
@@ -17,17 +13,6 @@ namespace Senai.Projeto.Carfel.Controllers
 
         [HttpPost]
         public IActionResult Comentar(IFormCollection form){
-<<<<<<< HEAD
-=======
-            UsuarioModel usuarioModel = new UsuarioModel();
-
-            ComentarioModel comentario = new ComentarioModel(
-                conteudo: form["conteudo"],
-                nome: usuarioModel.Nome,
-                dataCriacao: DateTime.Now
-            );
-
->>>>>>> 80754a9ef13e3b31921480b933bee5108c855348
             return null;
         }
     }
